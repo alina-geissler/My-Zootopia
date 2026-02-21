e78e4e1d90e71f99fb17d18e151da00f8cd461e1
alina-geissler <alina.geissler@t-online.de>
2026-02-22 00:04:42 +0100

    create a function for serialising a single animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -18,28 +18,36 @@ def load_template(file_path):
 
 template = load_template("animals_template.html")
 
-# create a string with the animal data
-output = ''
 
-for animal in animals_data:
+def serialize_animal(animal_obj):
+    """
+    Create HTML card element for an animal
+    :param animal_obj: information to use for card element
+    :return: HTML code for card element
+    """
+    output = ''
     output += '<li class="cards__item">\n'
-    output += f'<div class="card__title">{animal["name"]}</div>\n'
+    output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
     output += '<p class="card__text">'
-    diet = animal['characteristics'].get('diet')
+    diet = animal_obj['characteristics'].get('diet')
     if diet:
         output += f'<strong>Diet:</strong> {diet}<br/>\n'
-    locations = animal['locations']
+    locations = animal_obj['locations']
     if locations:
         output += f'<strong>Location:</strong> {locations[0]}<br/>\n'
-    animal_type = animal['characteristics'].get('type')
+    animal_type = animal_obj['characteristics'].get('type')
     if animal_type:
         output += f'<strong>Type:</strong> {animal_type}<br/>\n'
     output += '</p>\n'
     output += '</li>\n'
+    return output
 
+
+output = ''
+for animal_obj in animals_data:
+    output += serialize_animal(animal_obj)
 
 html_with_data = template.replace("__REPLACE_ANIMALS_INFO__", output)
-print(html_with_data)
 
-with open("animals.html", "w") as handle:
+with open("animals.html", "w", encoding="utf-8") as handle:
     handle.write(html_with_data)
